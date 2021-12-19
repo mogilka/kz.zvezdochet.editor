befe14712cd175db1111cbf798bad706d38c9fe0
natalie <natalie@natalie-Inspiron-3847>
2021-12-19 13:25:18 +0600

    Отлов исключения

--- src/kz/zvezdochet/editor/handler/MainHandler.java
+++ src/kz/zvezdochet/editor/handler/MainHandler.java
@@ -17,8 +17,12 @@ public class MainHandler extends Handler {
 	
 	@Execute
 	public void execute(MApplication app, EModelService service, EPartService partService) {
-		MPerspective perspective = (MPerspective)service.find("kz.zvezdochet.editor.perspective.dict", app);
-		if (perspective != null)
-			partService.switchPerspective(perspective);
+		try {
+			MPerspective perspective = (MPerspective)service.find("kz.zvezdochet.editor.perspective.dict", app);
+			if (perspective != null)
+				partService.switchPerspective(perspective);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
